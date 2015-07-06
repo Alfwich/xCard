a4de2ea58d512969f01a6e5ab4b752baeec25a46
Arthur Wuterich <arthurwut@gmail.com>
2015-07-06 15:21:22 -0700

    Wraps mainPage JS file in a anon function to start with processing functions.

--- scripts/README.md
+++ scripts/README.md
@@ -2,7 +2,8 @@
 Importing into application
 ==========================
 
-Use this command from the script folder, with the Meteor server running, to import the cards into the Cards collection
+Use this command from the script folder, with the Meteor server running, to import the cards into the Cards collection.
+You will need the mongo tools installed to use the mongoimport command.
 
 ```
 mongoimport -h localhost:3001 --drop --db meteor --collection Cards --type json --file cards.json --jsonArray
--- xcard-meteor/client/views/main/main.js
+++ xcard-meteor/client/views/main/main.js
@@ -1,29 +1,32 @@
-Template.mainPage.events({
-	"click button" : function(e) {
-		var val = xCard.Session.get("main.helloCounter");
+(function(){
+	var formatCardData = function()
+	Template.mainPage.events({
+		"click button" : function(e) {
+			var val = xCard.Session.get("main.helloCounter");
 
-		if( _.isNumber(val) ) {
-			xCard.Session.set( "main.helloCounter", val+1 );
+			if( _.isNumber(val) ) {
+				xCard.Session.set( "main.helloCounter", val+1 );
+			}
 		}
-	}
-});
+	});
 
-Template.mainPage.helpers({
-	helloCounter : function() {
-		return xCard.Session.get( "main.helloCounter" );
-	},
-	cards: function() {
-		var result = [],
-				cards = CardModel.find();
+	Template.mainPage.helpers({
+		helloCounter : function() {
+			return xCard.Session.get( "main.helloCounter" );
+		},
+		cards: function() {
+			var result = [],
+					cards = CardModel.find();
 
-		if( cards ) {
-			result = cards.fetch();
+			if( cards ) {
+				result = cards.fetch();
+			}
+
+			return result;
 		}
+	});
 
-		return result;
+	Template.mainPage.rendered = function() {
+		xCard.Session.set( "main.helloCounter", 0 );
 	}
-});
-
-Template.mainPage.rendered = function() {
-	xCard.Session.set( "main.helloCounter", 0 );
-}
+})();
