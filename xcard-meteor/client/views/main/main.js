e7913d4685e6a5a867935103df3e7c19c0f597ee
Arthur Wuterich <arthurwut@gmail.com>
2015-07-07 10:31:01 -0700

    Removed Hello counter example button.

--- xcard-meteor/client/views/main/main.js
+++ xcard-meteor/client/views/main/main.js
@@ -1,35 +1,21 @@
 (function(){
-	var mainCounterName = "main.helloCounter";
 
 	Template.mainPage.events({
-		"click button.hello" : function(e) {
-			var val = xCard.Session.get(mainCounterName);
-
-			if( _.isNumber(val) ) {
-				xCard.Session.set( mainCounterName, val+1 );
-			}
-		}
 	});
 
 	Template.mainPage.helpers({
-		helloCounter : function() {
-			return xCard.Session.get(mainCounterName);
+		numberOfCards: function() {
+			return CardsCollection.find().count();
 		},
 
 		cards: function() {
-			var result = [],
-					cards = CardsCollection.find();
-
-			if( cards.count() ) {
-				// Create an array of Card objects from the result of the cards query
-				result = cards.fetch().map( function(ele){ return new CardModel(ele); });
-			}
-
+			var result = CardsCollection.find().fetch();
+			// Create a CardModel from each result of the query
+			result = result.map( function(ele){ return new CardModel(ele); });
 			return result;
 		}
 	});
 
 	Template.mainPage.rendered = function() {
-		xCard.Session.set(mainCounterName, 0 );
 	}
 })();
