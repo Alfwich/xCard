78b33172708225c32038325d384b7196418c09b7
thequestatredtotal <thequestatredtotal@gmail.com>
2015-07-07 20:52:17 -0700

    Simple schedule choosing. This assumes that each PLAYER is an ACTOR. #1

--- engine/main.py
+++ engine/main.py
@@ -3,6 +3,24 @@ class Player:
     def __init__(self, name):
         self.name = name
         self.cards = []
+        self.schedule = []
+
+    def chooseSchedule(self):
+        print("{}, please choose your schedule.".format(self.name))
+        print("[n] for nothing.")
+        i = 1
+        for card in self.cards:
+            print("[{}] for Card(\"{}\")".format(i, card.name))
+            ++i
+        choice = input("Enter your choice: ")
+        if choice == "n":
+            self.schedule = []
+            return
+
+        choiceInt = int(choice)
+        choiceIndex = choiceInt - 1
+        self.schedule = [self.cards[choiceIndex]]
+        del self.cards[choiceIndex]
 
     def addCardToHand(self, card):
         self.cards.append(card)
@@ -23,8 +41,8 @@ class Game:
 def main():
     players = [Player("Alan"), Player("Betty")]
 
-    for player in players:
-        player.addCardToHand(Card("Punch"))
+    players[0].addCardToHand(Card("Punch"))
+    players[1].addCardToHand(Card("Health Potion"))
 
     for player in players:
         print("Player: {}".format(player.name))
@@ -34,6 +52,12 @@ def main():
     # decide to not redraw
 
     game = Game(players)
+
+    while True:
+        for player in players:
+            player.chooseSchedule()
+            print("{}.schedule = {}".format(player.name, player.schedule))
+        break
 
 if __name__ == "__main__":
     main()
