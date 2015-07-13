e02e7ca520f35cd49f3e65c25623a193fa2ca8d0
Arthur Wuterich <arthurwut@gmail.com>
2015-07-13 10:24:25 -0700

    Adds 'addCard' and 'removeCard' to DeckModel. This will coordinate adding cards to a deck. This will allow for some more complicated add/remove behavior for the cards on the client based on cardId rather than ownershipId. This is preferred over pushing it on the server because there will still be a validation step to prevent duplicate cards entries and performing more calculations clientside is more scaleable( and exhibits Meteor's distributed methodology.

--- xcard-meteor/lib/models/DeckModel.js
+++ xcard-meteor/lib/models/DeckModel.js
@@ -4,16 +4,36 @@ DeckModel = function(raw) {
   this.cards = _.get(raw,"cards", []);
   this.name = _.get(raw,"name", "New Deck");
 
+  // Generate an array of CardModels for each card in the deck
   this.cards = _.filter( _.map( this.cards, function(ele){
     var ownership = CardOwnershipCollection.findOne( ele ),
-        card = CardsCollection.findOne( ownership.cardId ),
         result = "";
 
-      if( ownership && card ) {
-        result = new CardModel( card );
-        result.ownershipId = ownership._id;
+    if( ownership ) {
+      var card = CardsCollection.findOne( ownership.cardId );
+
+        if( ownership && card ) {
+          result = new CardModel( card );
+          result.ownershipId = ownership._id;
+        }
+      } else {
+        console.warn("Attempted to unpack deck with invalid cardId: " + ele);
       }
 
       return result;
   }));
+
+  this.addCard = function( ownershipId ) {
+    //TODO: Make this function with a cardId rather than an ownershipId.
+    //      This would work such that a check would be made to see if there is
+    //      an available card from the user's owned cards, thats NOT already
+    //      present in the current deck, and add it to this deck.
+    Meteor.call("addCardToDeck", this._id, ownershipId);
+  };
+
+  this.removeCard = function( ownershipId ) {
+    //TODO: Make this function with a cardId rather than an ownershipId.
+    //      See Above
+    Meteor.call("removeCardFromDeck", this._id, ownershipId);
+  }
 }
