4266be08b2bdb3f21be64113b32cc4d091c40926
Arthur Wuterich <arthurwut@gmail.com>
2015-07-13 10:32:12 -0700

    Seperates the deck component's two views: 'detailed', 'overview' into two seperate templates. This is to facilitate modularity.

--- xcard-meteor/client/components/deck/deck.js
+++ xcard-meteor/client/components/deck/deck.js
@@ -1,9 +1,19 @@
-Template.deck.helpers({
-  name: function() {
+Template.deckOverview.helpers({
+  name: function(){
     return this.name;
   },
 
   cards: function() {
     return this.cards;
   }
-})
+});
+
+Template.deckDetailed.helpers({
+  name: function(){
+    return this.name;
+  },
+
+  cards: function() {
+    return this.cards;
+  }
+});
