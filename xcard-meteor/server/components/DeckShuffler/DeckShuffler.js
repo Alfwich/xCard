1a958b815ae4688a0174e63bcb00ca45a2194007
Arthur Wuterich <arthurwut@gmail.com>
2015-07-16 15:15:52 -0700

    Adds DeckShuffler server component. This function takes a deckId and returns an array of cardIds where this is a shuffled version of the deck's cards.

---
+++ xcard-meteor/server/components/DeckShuffler/DeckShuffler.js
@@ -0,0 +1,27 @@
+// Returns an array of cardIds for a provided deck. The length of the array
+// will be the number of cards in the deck. The order of the cardIds are randomly
+// generated to simulate a random shuffle of the deck's cards.
+DeckShuffler = function(deckId) {
+  var result = [],
+      deck = UserDeckCollection.findOne(deckId);
+
+  if(deck) {
+    var cardIds = _.map(deck.cards, function(ele){
+        var ownership = CardOwnershipCollection.findOne( ele.ownershipId );
+
+        if(ownership) {
+          var card = CardCollection.findOne( ownership.cardId )
+
+          if(card) {
+            // Add the number of cards to the array equal to the number of cards
+            // of this type in the deck
+            for( var i = 0; i < ele.count; i++ ) {
+              result.push(card._id);
+            }
+          }
+        }
+    });
+  }
+
+  return _.shuffle(result);
+}
