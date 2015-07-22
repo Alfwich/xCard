a7955828f2e55fadb49e56e0f6fae2676a47b0fd
Arthur Wuterich <arthurwut@gmail.com>
2015-07-21 23:44:28 -0700

    Adds two more card definitions

--- xcard-meteor/server/game/main.cards.js
+++ xcard-meteor/server/game/main.cards.js
@@ -1,5 +1,20 @@
-xCard.cardEvaluator.registerCard( "Dead At The Mist", function( game, action ) {
+xCard.cardEvaluator.registerCard("Dead At The Mist", function(game, action) {
   game.addGlobalGameMessage( action.player.playerName + " breathed in death at the mist!" ); 
   game.modifyPlayerValue( action.playerGameId, "health", -1 );
   return true;
+});
+
+xCard.cardEvaluator.registerCard("Belonging To Myself", function(game,action) {
+  game.addGlobalGameMessage( action.player.playerName + " crashed down with some phat philosophical banter!" ); 
+  _.each( game.playersMap, function(ele) {
+    game.modifyPlayerValue( ele, "health", -1 );
+  });
+});
+
+
+xCard.cardEvaluator.registerCard("Assassins Of History", function(game, action) {
+  game.addGlobalGameMessage( action.player.playerName + " stabbed a random player!" ); 
+  var randomPlayerId = _.random(1,game.state.totalPlayers);
+  console.log( randomPlayerId );
+  game.modifyPlayerValue( randomPlayerId, "health", -3 );
 });
