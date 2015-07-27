08d39e6ffc75f78aad72669360ff0cbb6cf2e8a1
Arthur Wuterich <arthurwut@gmail.com>
2015-07-27 15:35:37 -0700

    Adds addTarget and removeTarget global method. addTarget and removeTarget will add ad provided ID and name as a targetable.  This is called when a player is added or removed.

--- xcard-meteor/server/game/models/Game/Game.js
+++ xcard-meteor/server/game/models/Game/Game.js
@@ -7,12 +7,11 @@ var get = function( obj, k, def ) {
 }
 
 Game = function(raw) {
-
   this.creator = get(raw, "creator");
   this.playersMap = get(raw, "playersMap", {});
   this.players = get(raw, "players", {});
   this.options = get(raw, "options", {});
   this.state = get(raw, "state", { current: "init", activePlayer: 1, totalPlayers: 0 });
   this.messages = get(raw, "messages", []);
-  this.targets = {};
+  this.targets = get(raw, "targets", {});
 }
