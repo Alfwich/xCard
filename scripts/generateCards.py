e73772e3c96d5d4f1abc1d7f43205db5b4f2f609
Arthur Wuterich <arthurwut@gmail.com>
2015-07-27 17:46:11 -0700

    Adds ability to cast multiple cards per main state. Players will lose mana equal to the cost of the card's manaCost. If a player does not have enough mana the card's effects will not take place. Card generation script beefed up to include number of targets per card. Card target count is checked when the card is being casted and will only ask for targets if they are required.

--- scripts/generateCards.py
+++ scripts/generateCards.py
@@ -1,37 +1,37 @@
 import json, random, sys
 
 OUT_FILE = "cards.json"
-RANDOM_TITLES = [
-	"Heir With Gold",
-	"Duchess Of Greatness",
-	"Assassins Of History",
-	"Humans Of Time",
-	"Gangsters And Guardians",
-	"Serpents And Lords",
-	"Demise Of Twilight",
-	"Shield Of Hell",
-	"Searching At Secrets",
-	"Dead At The Mist",
-	"Wife Without A Home",
-	"Swindler Without Shame",
-	"Lions With Silver",
-	"Friends Of Reality",
-	"Friends And Serpents",
-	"Snakes And Strangers",
-	"Failure Of Freedom",
-	"Foundation Of History",
-	"Visiting The World",
-	"Death At The Void",
-	"Raven Of Silver",
-	"Opponent With Honor",
-	"Butchers Of The Mountain",
-	"Invaders Of Sorrow",
-	"Enemies And Officers",
-	"Snakes And Guardians",
-	"Fruit Without Faith",
-	"Goal Of Silver",
-	"Sounds In My Past",
-	"Belonging To Myself"
+CARDS = [
+	[ "Heir With Gold", 0 ],
+	[ "Duchess Of Greatness", 0 ],
+	[ "Assassins Of History", 0 ],
+	[ "Humans Of Time", 0 ],
+	[ "Gangsters And Guardians", 0 ],
+	[ "Serpents And Lords", 0 ],
+	[ "Demise Of Twilight", 0 ],
+	[ "Shield Of Hell", 0 ],
+	[ "Searching At Secrets", 0 ],
+	[ "Dead At The Mist", 0 ],
+	[ "Wife Without A Home", 0 ],
+	[ "Swindler Without Shame", 0 ],
+	[ "Lions With Silver", 0 ],
+	[ "Friends Of Reality", 0 ],
+	[ "Friends And Serpents", 0 ],
+	[ "Snakes And Strangers", 0 ],
+	[ "Failure Of Freedom", 0 ],
+	[ "Foundation Of History", 0 ],
+	[ "Visiting The World", 0 ],
+	[ "Death At The Void", 0 ],
+	[ "Raven Of Silver", 0 ],
+	[ "Opponent With Honor", 0 ],
+	[ "Butchers Of The Mountain", 0 ],
+	[ "Invaders Of Sorrow", 0 ],
+	[ "Enemies And Officers", 1 ],
+	[ "Snakes And Guardians", 0 ],
+	[ "Fruit Without Faith", 0 ],
+	[ "Goal Of Silver", 0 ],
+	[ "Sounds In My Past", 0 ],
+	[ "Belonging To Myself", 0]
 ]
 
 def genId():
@@ -42,18 +42,19 @@ genId.current = (16**4)-1
 genId.prefix = ''.join([random.choice('0123456789abcdef') for x in range(19)])
 
 
-def genCard(name):
+def genCard(name, targets):
 	result = {
 		"_id": genId(),
 		"title": name,
 		"body": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius metus quis tempus venenatis. Ut non ultricies mi, a blandit lorem. Vestibulum blandit sem et sem semper maximus. Vestibulum quis accumsan odio, vel eleifend elit. Curabitur ultrices risus ac auctor malesuada. Aenean accumsan leo at elit commodo faucibus. Praesent tempus enim vitae fringilla faucibus. Pellentesque sodales maur",
 		"image": "test0%d.jpg" % ( random.randint(1,9) ),
-		"targets" : 1
+		"targets": targets,
+		"manaCost": 1
 	}
 	return result
 
 def main():
-	cards = [genCard(x) for x in RANDOM_TITLES]
+	cards = [genCard(x[0],x[1]) for x in CARDS]
 	with open( OUT_FILE, "w" ) as f:
 		json.dump(cards,f)
 
